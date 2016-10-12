9456d9eb759cb172d139ed4b06339357685ffcc7
Maxwell Nderitu <nderitumaxwell@gmail.com>
2016-10-12 16:40:58 +0300

    Added editing pages functionality

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte= timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -35,3 +35,25 @@ def post_new(request):
         form = PostForm()
 
     return render(request, 'blog/post_new.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    # Check if the form has been submitted
+    if request.method == 'POST':
+        # Get the form
+        form = PostForm(request.POST, instance=post)
+
+        # CHeck if the form os valid
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form': form})
